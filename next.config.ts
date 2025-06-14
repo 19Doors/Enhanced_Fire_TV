1b80432d14deb9e9510e321bd7bc8133bfaadf45
19Doors <sakaarsri1904@gmail.com>
2025-06-14 23:47:29 +0530

    Basic OTTs_Prod

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
